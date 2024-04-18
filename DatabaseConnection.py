8fc6cf88e2f653c65d683f5e75110dd3f2a108f7
syedaquibali <aquib001ali@gmail.com>
2024-04-19 01:00:52 +0530

    crud fast api

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-sql_database_url = "postgres://crud_api_fast_user:e0X89N0xEsW264IWJfZYpzkIGZo1SB1Q@dpg-cogn2fo21fec73bj2if0-a/crud_api_fast"
+sql_database_url = "postgresql://crud_api_fast_user:e0X89N0xEsW264IWJfZYpzkIGZo1SB1Q@dpg-cogn2fo21fec73bj2if0-a/crud_api_fast"
 
 engine = create_engine(sql_database_url)
 
